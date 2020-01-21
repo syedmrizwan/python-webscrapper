de1af16d1430b0c406f4f7aaa6980db8e5abbf2b
syedmrizwan <syed.m.rizwan@outlook.com>
2020-01-21 23:00:32 +0500

    Webscrapper first commit

---
+++ index.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://www.monster.com/jobs/search/?q=Software-Developer&where=Australia'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='ResultsContainer')
+
+print(results.prettify())
+
+
+job_elems = results.find_all('section', class_='card-content')
+
+for job_elem in job_elems:
+    # Each job_elem is a new BeautifulSoup object.
+    # You can use the same methods on it as you did before.
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    print(title_elem)
+    print(company_elem)
+    print(location_elem)
+    print()
